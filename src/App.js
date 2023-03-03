fb51a84ce74efdedd916ce804ddf3908860f8b54
Fernanda Winter <fernanda.winter@hotmail.com>
2023-03-03 19:54:54 -0300

    creating a paragraph and a button component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import Title from './components/Title';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Title from "./components/Title";
+import Paragraph from "./components/Paragraph";
+import Button from "./components/Button";
 
 function App() {
+  function onClickFunction() {
+    alert("You clicked on me!");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Title text="pagina1"/>
-        <h1>
-          Change
-        </h1>
-
+        <Title text="my Title" />
+        <Paragraph text="my Paragraph" />
+        <Button onClick={onClickFunction} text="my Button" />
+        <h1>Change</h1>
       </header>
     </div>
   );
