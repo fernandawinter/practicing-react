0a86357effcdbae928664d7591fb5a0c50bf1612
Fernanda Winter <fernanda.winter@hotmail.com>
2023-03-03 19:07:14 -0300

    first commit - creating a title component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title text="pagina1"/>
+        <h1>
+          Change
+        </h1>
+
       </header>
     </div>
   );
