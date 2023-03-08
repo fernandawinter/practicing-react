696bfe2e08a14577c88ee9fb04eb296589dc05da
Fernanda Winter <fernanda.winter@hotmail.com>
2023-03-08 03:36:21 -0300

    changing for css-in-js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,20 @@ import Title from "./Title";
 import Paragraph from "./Paragraph";
 import Button from "./Button";
 
+const cardContainerStyle = {
+  width: '300px',
+  overflow: 'hidden',
+  boxShadow: '0px 0px 15px -5px',
+  transition: '0.3s',
+  animation: 'ease-in',
+  padding: '10px',
+  borderRadius: '20px',
+  border: 'solid 2px',
+};
+
 const Card = ({ firstButtonOnClick, secondButtonOnClick, title, firstParagraph, secondParagraph, firstButton, secondButton }) => {
   return (
-    <div className="card-container">
+    <div style={cardContainerStyle}>
       <div className="card-content">
         <Title text={title}/>
         <Paragraph text={firstParagraph}/>
--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -3,7 +3,7 @@ import React from "react";
 const Paragraph = (props) => {
     return (    
     <div>
-        <h1>{props.text}</h1>
+        <p>{props.text}</p>
     </div>
     );
 }
